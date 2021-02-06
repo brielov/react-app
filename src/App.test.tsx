bd0dfa921e6352ada2256d01206ee87df2e38ad4
Gabriel Vaquer <brielov@icloud.com>
2021-02-06 09:10:02 -0300

    Change App test description

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from "@testing-library/react";
 
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders hello world message", () => {
   render(<App />);
   const linkElement = screen.getByText(/hello world/i);
   expect(linkElement).toBeInTheDocument();
