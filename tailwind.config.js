3ee38c9fc2d66d89566ae154f3f97523a79dfab7
Gabriel Vaquer <brielov@icloud.com>
2021-02-22 11:16:11 -0300

    Upgrade dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ["./src/**/*.tsx", "public/index.html"],
+  purge: ["src/**/*.tsx", "public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
