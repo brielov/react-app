101dc8923fa094521ae78140cd3cc2e8d41f4b14
Gabriel Vaquer <brielov@icloud.com>
2021-04-01 21:34:00 -0300

    Disable compression in development

--- webpack.config.js
+++ webpack.config.js
@@ -100,25 +100,28 @@ module.exports = {
     new CleanWebpackPlugin(),
     new DotenvWebpackPlugin(),
 
-    new CompressionPlugin({
-      filename: "[path][base].gz",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-    new CompressionPlugin({
-      filename: "[path][base].br",
-      algorithm: "brotliCompress",
-      test: /\.(js|css|html|svg)$/,
-      compressionOptions: {
-        params: {
-          [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+    isProd &&
+      new CompressionPlugin({
+        filename: "[path][base].gz",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+
+    isProd &&
+      new CompressionPlugin({
+        filename: "[path][base].br",
+        algorithm: "brotliCompress",
+        test: /\.(js|css|html|svg)$/,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
         },
-      },
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
 
     new CopyPlugin({
       patterns: [
