08fb22ab878c995f3211239f9b446ae4a891a25f
Gabriel Vaquer <brielov@icloud.com>
2022-02-09 10:20:09 -0300

    Make template PWA friendly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["index.html", "src/**/*.tsx"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        standalone: { raw: "(display-mode: standalone)" },
+      },
+    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
