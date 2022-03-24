280dd4bfc4abe75214e08aa325d7321617298625
Gabriel Vaquer <brielov@icloud.com>
2022-03-24 17:47:12 -0300

    Add vite-plugin-checker

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), checker({ typescript: true })],
   build: {
     target: "es2020",
   },
