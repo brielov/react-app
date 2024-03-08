0db9d3f3b256fc36ef3a3d76d6bfc2bf1cfc1612
Gabriel Vaquer <brielov@icloud.com>
2024-03-08 10:37:30 -0300

    Updated vite config to disable emitting legal comments when building

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { defineConfig } from "vitest/config";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), tailwindcss()],
+  esbuild: { legalComments: "none" },
   test: {
     globals: true,
     environment: "jsdom",
