9ecf72cb88f73289a3711ea84f4c004fbf5c1413
Gabriel Vaquer <brielov@icloud.com>
2024-03-08 09:08:44 -0300

    Switched from eslint and prettier to biomejs. Switched to tailwind v4-alpha

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react(), tailwindcss()],
   test: {
     globals: true,
     environment: "jsdom",
